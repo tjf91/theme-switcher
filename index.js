712ab7d9255dcdc977cfe7ff32b198ae3e1406f8
tjf91 <tudorjflorea@gmail.com>
2020-11-12 05:45:00 -0700

    done

--- index.js
+++ index.js
@@ -22,5 +22,16 @@ console.log(element)
 
 
 const selectTheme = (theme) => {
-    document.getElementsByTagName('body')
+    const body = document.getElementsByTagName('body')[0]
+    body.className = theme
+    const main = document.getElementsByTagName('main')[0]
+    main.className = theme
+
+    const buttons = document.getElementsByTagName("button")
+    for(let i=0;i<buttons.length;i++){
+        buttons[i].className = theme
+    }
+
+
 }
+
